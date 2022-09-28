114feaa27cdef2233d366512724c564dd1972e26
Matthias Harte <mharte@crcmail.net>
2022-09-27 22:56:51 -0400

    Implement Tests

--- PersonTests/PersonTestsStarterForm.cs
+++ PersonTests/PersonTestsStarterForm.cs
@@ -5,7 +5,6 @@ namespace PersonTests
     /// </summary>
     public partial class PersonTestsStarterForm : Form
     {
-        private int passedTests;
         private int failedTests;
 
         /// <summary>
@@ -16,17 +15,75 @@ namespace PersonTests
             this.InitializeComponent();
         }
 
+        private static int TestSize()
+        {
+            Person person = new ("Test");
+            if (person.Size == 4)
+            {
+                return 0;
+            }
+            else
+            {
+                MessageBox.Show("TestSize failed!");
+                return 1;
+            }
+        }
+
+        private static int TestReset()
+        {
+            Person person = new ("Test");
+            person.Reset();
+            if (person.Name == string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                MessageBox.Show("TestReset failed!");
+                return 1;
+            }
+        }
+
+        private static int TestCompare()
+        {
+            Person firstPerson = new ("Alice");
+            Person secondPerson = new ("Bob");
+            if (firstPerson.CompareTo(secondPerson) < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                MessageBox.Show("TestCompare failed!");
+                return 1;
+            }
+        }
+
+        private static int TestDisplay()
+        {
+            try
+            {
+                new Person("Bob").Display();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("TestDisplay failed!");
+                return 1;
+            }
+
+            return 0;
+        }
+
         private void RunTestsButton_Click(object sender, EventArgs e)
         {
-            this.passedTests = 0;
+            this.failedTests = 0;
 
-            // Add feature tests here
-            this.passedTests += this.TestDisplay();
-            this.passedTests += this.TestCompare();
-            this.passedTests += this.TestPrint();
-            this.passedTests += this.TestReset();
-
-            MessageBox.Show(this.passedTests.ToString() + " of 4 tests passed!");
+            // Run feature tests here
+            // this.failedTests += TestDisplay();
+            // this.failedTests += TestCompare();
+            // this.failedTests += TestReset();
+            // this.failedTests += TestSize();
+            MessageBox.Show(this.failedTests.ToString() + " of 4 tests failed!");
         }
     }
 }
